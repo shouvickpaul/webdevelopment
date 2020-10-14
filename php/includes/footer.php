4b9d3ad0e5bf977c38a6cd0457346b1ebec5e490
Shouvick Paul <67785523+shouvickpaul@users.noreply.github.com>
2020-10-14 14:11:55 +0530

    Add files via upload

---
+++ php/includes/footer.php
@@ -0,0 +1,30 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-4 text-center">
+              <h3>Information</h3>
+                <p><a href="about_us.php"><span style="color:white">About Us</span></a></p>
+                <p><a href="contact_us.php"><span style="color:white">Contact Us</span></a></p>
+            </div>
+
+            <div class="col-12 col-sm-4 text-center">
+              <h3>My Account</h3>
+                <p><a href="#" data-toggle="modal" data-target="#loginmodal" ><span style="color:white">Log In</span></a></p>
+                <p><a href="signup.php"><span style="color:white">Sign Up</span></a></p>
+            </div>
+
+            <div class="col-12 col-sm-4 text-center">
+              <h3>Contact Us</h3>
+                <p>CONTACT No.&nbsp;&nbsp;&nbsp;&nbsp;&#43;91&#45;123&#45;456&#45;7890</p>
+                <p>E&#45;Store&#46;com &copy;<?php echo DATE('Y') ;?>.&nbsp;&nbsp;All Rights Reserved.</p>
+            </div>
+        </div>
+        <div class="row">
+        	<div class="col-12 text-center">
+        		Copyright <span class="glyphicon glyphicon-copyright-mark"></span> Ecommerce Stores. All Rights Reserved | Contact us: +91 98765 43210
+        		<span><a class="link" href=#>Made with&nbsp;</a>&#x1f495;<a class="link" href="">&nbsp;in India</a></span>
+        		
+        	</div>		
+		</div>
+    </div>
+</footer>
---
+++ php/includes/header.php
@@ -0,0 +1,37 @@
+<?php
+        include 'includes/modal.php';
+        include 'includes/common.php';
+?>
+<nav class="nav navbar-inverse navbar-fixed-top">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mynavBar">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a href="index.php" class="navbar-brand">Ecommerce Stores</a>
+		</div>
+		<div class="collapse navbar-collapse" id="mynavBar">
+			<?php
+				if(isset($_SESSION["email_id"])){ 
+			?>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
+					<li><a href="settings.php"><span class="glyphicon glyphicon-user"></span> Settings</a></li>
+					<li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+				</ul>
+			<?php
+				}else{
+			?>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#" data-toggle="modal" data-target="#loginmodal"><span class="glyphicon glyphicon-log-in"></span> Log in</a></li>
+					<li><a href="signup.php"><span class="glyphicon glyphicon-user"></span> SignUp</a></li>
+				</ul>
+			<?php
+				}
+			?>
+		</div>
+	</div>
+</nav>
+			
